65d1df6c89e3a5239ae9e45e15946c3284cd3473
samuelfer <samuelfesant@gmail.com>
2022-06-22 16:17:29 -0300

    First commit

---
+++ android/app/src/main/kotlin/com/example/sou_programador/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sou_programador
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MeuApp());
+}
+
+class MeuApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 96, 4, 112),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                backgroundImage: AssetImage('imagens/foto.png'),
+                radius: 50.0,
+              ),
+              Text(
+                'Samuel Fernandes',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'LibreBaskerville',
+                ),
+              ),
+              Text(
+                'DESENVOLVEDOR FULLSTACK',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.purple.shade100,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 250.0,
+                child: Divider(
+                  color: Colors.purple.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    size: 25.0,
+                    color: Colors.purple,
+                  ),
+                  title: Text(
+                    '(83) 3263-1056',
+                    style: TextStyle(
+                      color: Colors.purple,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    size: 25.0,
+                    color: Colors.purple,
+                  ),
+                  title: Text(
+                    'samuelfesant@gmail.com',
+                    style: TextStyle(
+                      color: Colors.purple,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.gite,
+                    size: 25.0,
+                    color: Colors.purple,
+                  ),
+                  title: Text(
+                    'https://github.com/samuelfer',
+                    style: TextStyle(
+                      color: Colors.purple,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // Widget _card() {}
+}
